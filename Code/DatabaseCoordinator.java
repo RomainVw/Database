82c371f7c9bfb723c441101cd3899d2fccd37d61
Benjamin Baugnies <benjamin.baugnies@student.uclouvain.be>
2014-05-06 18:09:36 +0200

    tested db coordinator for charactermodels

--- Code/DatabaseCoordinator.java
+++ Code/DatabaseCoordinator.java
@@ -6,11 +6,20 @@ import java.sql.ResultSet;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import narrationManager.model.CharacterModel;
 
 
 public class DatabaseCoordinator
-{
+{    
+  /*public static void main(String[] args)
+  {
+    DatabaseCoordinator c = new DatabaseCoordinator();
+    CharacterModel c1 = c.getCharacter("Pierre");
+    System.out.println(c1.getName());
+  }*/
   private Connection con = null;
   
   public DatabaseCoordinator()
@@ -18,7 +27,12 @@ public class DatabaseCoordinator
     String url = "jdbc:postgresql://localhost/lingi2172";
     String user = "postgres";
     String pwd = "ben";
-    con = DriverManager.getConnection(url, user, pwd);    
+    try {
+      con = DriverManager.getConnection(url, user, pwd); 
+    } catch (SQLException e) {      
+        Logger lgr = Logger.getLogger(DatabaseCoordinator.class.getName());
+        lgr.log(Level.SEVERE, e.getMessage(), e);
+    }
   }
   
   public CharacterModel getCharacter(String name)
@@ -27,11 +41,16 @@ public class DatabaseCoordinator
     PreparedStatement pst = null;
     
     // get birth place
-    pst = con.prepareStatement("select PLACENAME from   ORIGINATES, CHARACTER, PLACE where NAME = ? and CHARACTER.CHARACTERID = ORIGINATES.CHARACTERID and ORIGINATES.PLACEID = PLACE.PLACEID");
-    pst.setString(1, name);
-    ResultSet res = pst.executeQuerry();
-    res.next();
-    b_place = res.getString(1);
+    try {
+      pst = con.prepareStatement("select PLACENAME from   ORIGINATES, CHARACTER, PLACE where NAME = ? and CHARACTER.CHARACTERID = ORIGINATES.CHARACTERID and ORIGINATES.PLACEID = PLACE.PLACEID");
+      pst.setString(1, name);
+      ResultSet res = pst.executeQuery();
+      res.next();
+      b_place = res.getString(1);
+    } catch (SQLException e) {      
+        Logger lgr = Logger.getLogger(DatabaseCoordinator.class.getName());
+        lgr.log(Level.SEVERE, e.getMessage(), e);
+    }
     
     return new CharacterModel(name, b_place);  
   }
@@ -41,16 +60,23 @@ public class DatabaseCoordinator
     PreparedStatement pst = null;
     
     // update birthplace
-    pst = con.prepareStatement("update originates set placeid = (select placeid from place where placename = ?) from character where character.characterid=originates.characterid and character.name=?");
-    pst.setString(1, c.getPlace());
-    pst.setString(2, c.getName());
-    pst.executeUpdate();
+    try {
+      pst = con.prepareStatement("update originates set placeid = (select placeid from place where placename = ?) from character where character.characterid=originates.characterid and character.name=?");
+      pst.setString(1, c.getB_place());
+      pst.setString(2, c.getName());
+      pst.executeUpdate();
+      
+      // update name
+      pst = con.prepareStatement("update CHARACTER set NAME = ? where NAME = ?");
+      pst.setString(1, c.getName());
+      pst.setString(2, c.update_db());
+      pst.executeUpdate(); 
+    } catch (SQLException e) {      
+        Logger lgr = Logger.getLogger(DatabaseCoordinator.class.getName());
+        lgr.log(Level.SEVERE, e.getMessage(), e);
+    }     
     
-    // update name
-    pst = con.prepareStatement("update CHARACTER set NAME = ? where NAME = ?");
-    pst.setString(1, c.getName());
-    pst.setString(2, c.update_db());
-    pst.executeUpdate();    
+
     
   }
 }
