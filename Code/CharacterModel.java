d4d7d549062a59af58afe8f2d96c6a22b2d65108
Benjamin Baugnies <benjamin.baugnies@student.uclouvain.be>
2014-05-06 17:19:08 +0200

    CharacterModel and dbcoord for it

--- Code/CharacterModel.java
+++ Code/CharacterModel.java
@@ -2,5 +2,41 @@ package narrationManager.model;
 
 public class CharacterModel
 {
-  //TODO
+  private String last_dbName = null;
+  private String name = null;
+  private String b_place = null;
+  
+  public CharacterModel(String name, String b_place)
+  {
+    last_dbName = name;
+    this.name = name;
+    this.b_place = b_place;
+  }
+  
+  public String getName()
+  {
+    return name;
+  }
+  
+  public String getB_place()
+  {
+    return b_place;
+  }
+  
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+  
+  public void setB_place(String b_place)
+  {
+    this.b_place = b_place;
+  }
+  
+  public String update_db()
+  {
+    String res = last_dbName;
+    last_dbName = name;
+    return res;
+  }
 }
--- Code/DatabaseCoordinator.java
+++ Code/DatabaseCoordinator.java
@@ -1,6 +1,56 @@
 package narrationManager.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import narrationManager.model.CharacterModel;
+
+
 public class DatabaseCoordinator
 {
-  //TODO centralisation des accès DB
+  private Connection con = null;
+  
+  public DatabaseCoordinator()
+  {
+    String url = "jdbc:postgresql://localhost/lingi2172";
+    String user = "postgres";
+    String pwd = "ben";
+    con = DriverManager.getConnection(url, user, pwd);    
+  }
+  
+  public CharacterModel getCharacter(String name)
+  {
+    String b_place = null;
+    PreparedStatement pst = null;
+    
+    // get birth place
+    pst = con.prepareStatement("select PLACENAME from   ORIGINATES, CHARACTER, PLACE where NAME = ? and CHARACTER.CHARACTERID = ORIGINATES.CHARACTERID and ORIGINATES.PLACEID = PLACE.PLACEID");
+    pst.setString(1, name);
+    ResultSet res = pst.executeQuerry();
+    res.next();
+    b_place = res.getString(1);
+    
+    return new CharacterModel(name, b_place);  
+  }
+  
+  public void setCharacter(CharacterModel c)
+  {
+    PreparedStatement pst = null;
+    
+    // update birthplace
+    pst = con.prepareStatement("update originates set placeid = (select placeid from place where placename = ?) from character where character.characterid=originates.characterid and character.name=?");
+    pst.setString(1, c.getPlace());
+    pst.setString(2, c.getName());
+    pst.executeUpdate();
+    
+    // update name
+    pst = con.prepareStatement("update CHARACTER set NAME = ? where NAME = ?");
+    pst.setString(1, c.getName());
+    pst.setString(2, c.update_db());
+    pst.executeUpdate();    
+    
+  }
 }
